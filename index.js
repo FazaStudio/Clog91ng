5621bdff1b590c11fd0a35e26c33eaf94ff5cac5
FazaStudio <fazastudiosapp@gmail.com>
2024-06-26 06:51:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const connection = require('./db'); // Menggunakan file konfigurasi MySQL
-
+require('dotenv').config();
 const { ethers } = require('ethers');
 const usdtABI = require('./usdt.json');
 
 // Setup Ethereum provider (contoh menggunakan Infura)
 
 async function main(){
-    
-    const UsdtAddress = '0x38A5e1e4C2e5CD4c91279b097e888737488a5EBc'; //pairaddress
-    
-    const provider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/ob1DzIlPhoWA8CHH9_MIwiPJAx1usvsC');
+    const UsdtAddress = process.env.pairAddress; //pairaddress
+    const provider = new ethers.providers.WebSocketProvider(process.env.WEB_SOCKET);
+    const contract = new ethers.Contract(UsdtAddress, usdtABI, provider);
 
-    const contract  = new ethers.Contract(UsdtAddress,usdtABI , provider);
     contract.on('Swap', (from, to, address, value, event) => {
-        let tokenContractAddress = '0x38A5e1e4C2e5CD4c91279b097e888737488a5EBc'; // Ganti dengan alamat kontrak token yang sesuai
+        let tokenContractAddress = process.env.contractAddress; // Ganti dengan alamat kontrak token yang sesuai
         
         if (from !== tokenContractAddress) {
             let formattedValue = ethers.utils.formatUnits(value, 9);
+            let integerValue = Math.floor(Number(formattedValue)); // Mengubah nilai menjadi integer
             let idToUpdate = 1;
-            let query = 'UPDATE batas SET jumlahToken = jumlahToken + ? WHERE id = ?';
-             let jumlahToken = [formattedValue, idToUpdate];
-             connection.query(query, jumlahToken, (err, result) => {
+            let query = process.env.query;
+            let jumlahToken = [integerValue, idToUpdate];
+            
+            connection.query(query, jumlahToken, (err, result) => {
                 if (err) {
                     console.error('Error saving value to MySQL: ' + err.stack);
                     return;
                 }
-                console.log('Value saved to MySQL:', formattedValue);
+                console.log('Value saved to MySQL:', integerValue);
             });
-
 
             let info = {
                 from: from,
                 to: to,
                 address: address,
-                value: formattedValue,
+                value: integerValue,
                 event: event,
             };
             console.log(JSON.stringify(info, null, 4));
         }
     });
-    
-
 }
+
 main();
